4a7a9d462e5375737a33b8981b45edbf6b0717cc
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2023-01-08 16:32:57 +0900

    run_libを変更

--- run_lib.py
+++ run_lib.py
@@ -20,11 +20,10 @@ import gc
 import io
 import os
 import time
-
 import numpy as np
 import logging
+from PIL import Image
 # Keep the import below for registering all model definitions
-from models import ddpm, ncsnv2, ncsnpp
 import losses
 import sampling
 from models import utils as mutils
@@ -35,8 +34,8 @@ import likelihood
 import sde_lib
 from absl import flags
 import torch
-from torch.utils import tensorboard
 import tensorflow as tf
+from torch.utils import tensorboard
 from torchvision.utils import make_grid, save_image
 from utils import save_checkpoint, restore_checkpoint
 
@@ -56,17 +55,15 @@ def train(config, workdir):
   sample_dir = os.path.join(workdir, "samples")
   tf.io.gfile.makedirs(sample_dir)
 
-  tb_dir = os.path.join(workdir, "tensorboard")
-  tf.io.gfile.makedirs(tb_dir)
-  writer = tensorboard.SummaryWriter(tb_dir)
-
-  # Initialize model.
+  # Initialize model.　モデルの生成
   score_model = mutils.create_model(config)
-  ema = ExponentialMovingAverage(score_model.parameters(), decay=config.model.ema_rate)
-  optimizer = losses.get_optimizer(config, score_model.parameters())
+  """このemaとは何者なのか分からない"""
+  ema = ExponentialMovingAverage(score_model.parameters(), decay=config.model.ema_rate) #(ファイル先) ./models/ema.py 
+  optimizer = losses.get_optimizer(config, score_model.parameters()) #(ファイル先) ./losses.py
+  """このdictはdictionaryを作るpythonの関数"""
   state = dict(optimizer=optimizer, model=score_model, ema=ema, step=0)
 
-  # Create checkpoints directory
+  # Create checkpoints directory 
   checkpoint_dir = os.path.join(workdir, "checkpoints")
   # Intermediate checkpoints to resume training after pre-emption in cloud environments
   checkpoint_meta_dir = os.path.join(workdir, "checkpoints-meta", "checkpoint.pth")
@@ -76,16 +73,19 @@ def train(config, workdir):
   state = restore_checkpoint(checkpoint_meta_dir, state, config.device)
   initial_step = int(state['step'])
 
-  # Build data iterators
+  # Build data iterators データセットを作成
   train_ds, eval_ds, _ = datasets.get_dataset(config,
                                               uniform_dequantization=config.data.uniform_dequantization)
+  
+  
   train_iter = iter(train_ds)  # pytype: disable=wrong-arg-types
   eval_iter = iter(eval_ds)  # pytype: disable=wrong-arg-types
+
   # Create data normalizer and its inverse
   scaler = datasets.get_data_scaler(config)
   inverse_scaler = datasets.get_data_inverse_scaler(config)
 
-  # Setup SDEs
+  # Setup SDEs(SDEの設定)
   if config.training.sde.lower() == 'vpsde':
     sde = sde_lib.VPSDE(beta_min=config.model.beta_min, beta_max=config.model.beta_max, N=config.model.num_scales)
     sampling_eps = 1e-3
@@ -96,41 +96,56 @@ def train(config, workdir):
     sde = sde_lib.VESDE(sigma_min=config.model.sigma_min, sigma_max=config.model.sigma_max, N=config.model.num_scales)
     sampling_eps = 1e-5
   else:
-    raise NotImplementedError(f"SDE {config.training.sde} unknown.")
+    print("yaa")
+    #raise NotImplementedError(f"SDE {config.training.sde} unknown.")
 
   # Build one-step training and evaluation functions
   optimize_fn = losses.optimization_manager(config)
   continuous = config.training.continuous
   reduce_mean = config.training.reduce_mean
   likelihood_weighting = config.training.likelihood_weighting
+  
   train_step_fn = losses.get_step_fn(sde, train=True, optimize_fn=optimize_fn,
                                      reduce_mean=reduce_mean, continuous=continuous,
                                      likelihood_weighting=likelihood_weighting)
   eval_step_fn = losses.get_step_fn(sde, train=False, optimize_fn=optimize_fn,
                                     reduce_mean=reduce_mean, continuous=continuous,
                                     likelihood_weighting=likelihood_weighting)
-
-  # Building sampling functions
+  
+  # Building sampling functions 
   if config.training.snapshot_sampling:
     sampling_shape = (config.training.batch_size, config.data.num_channels,
-                      config.data.image_size, config.data.image_size)
+                      config.data.image_size, config.data.image_size, config.data.image_size)
     sampling_fn = sampling.get_sampling_fn(config, sde, sampling_shape, inverse_scaler, sampling_eps)
 
   num_train_steps = config.training.n_iters
 
   # In case there are multiple hosts (e.g., TPU pods), only log to host 0
   logging.info("Starting training loop at step %d." % (initial_step,))
-
-  for step in range(initial_step, num_train_steps + 1):
+  
+  
+    
+  
+  for step in range(0, num_train_steps + 1):
     # Convert data to JAX arrays and normalize them. Use ._numpy() to avoid copy.
     batch = torch.from_numpy(next(train_iter)['image']._numpy()).to(config.device).float()
+    #print(batch.shape)
     batch = batch.permute(0, 3, 1, 2)
     batch = scaler(batch)
+    batch = torch.unsqueeze(batch,dim=-4)  #  add channel axis for debug
+    #print(batch.shape)
+    """バッチの詳細を表示するためのもの
+    for i in range(3):
+      for j in range(32):
+        print(batch[0][i][j])
+  
+    batch = scaler(batch)"""
+
+
     # Execute one training step
     loss = train_step_fn(state, batch)
     if step % config.training.log_freq == 0:
       logging.info("step: %d, training_loss: %.5e" % (step, loss.item()))
-      writer.add_scalar("training_loss", loss, step)
 
     # Save a temporary checkpoint to resume training after pre-emption periodically
     if step != 0 and step % config.training.snapshot_freq_for_preemption == 0:
@@ -141,9 +156,9 @@ def train(config, workdir):
       eval_batch = torch.from_numpy(next(eval_iter)['image']._numpy()).to(config.device).float()
       eval_batch = eval_batch.permute(0, 3, 1, 2)
       eval_batch = scaler(eval_batch)
+      eval_batch = torch.unsqueeze(eval_batch,dim=-4)  #  add channel axis for debug
       eval_loss = eval_step_fn(state, eval_batch)
       logging.info("step: %d, eval_loss: %.5e" % (step, eval_loss.item()))
-      writer.add_scalar("eval_loss", eval_loss.item(), step)
 
     # Save a checkpoint periodically and generate samples if needed
     if step != 0 and step % config.training.snapshot_freq == 0 or step == num_train_steps:
@@ -153,19 +168,35 @@ def train(config, workdir):
 
       # Generate and save samples
       if config.training.snapshot_sampling:
-        ema.store(score_model.parameters())
-        ema.copy_to(score_model.parameters())
-        sample, n = sampling_fn(score_model)
-        ema.restore(score_model.parameters())
-        this_sample_dir = os.path.join(sample_dir, "iter_{}".format(step))
-        tf.io.gfile.makedirs(this_sample_dir)
-        nrow = int(np.sqrt(sample.shape[0]))
-        image_grid = make_grid(sample, nrow, padding=2)
-        sample = np.clip(sample.permute(0, 2, 3, 1).cpu().numpy() * 255, 0, 255).astype(np.uint8)
-        with tf.io.gfile.GFile(
-            os.path.join(this_sample_dir, "sample.np"), "wb") as fout:
-          np.save(fout, sample)
+        print("サンプリングを行います")
+        if config.data.dataset == "SLICE":
+          ema.store(score_model.parameters())
+          ema.copy_to(score_model.parameters())
+          sample, n = sampling_fn(score_model)
+          ema.restore(score_model.parameters())
+          this_sample_dir = os.path.join(sample_dir, "iter_{}".format(step))
+          tf.io.gfile.makedirs(this_sample_dir)
+          sample = np.clip(sample.cpu().numpy() * 255, 0, 255).astype(np.uint8) #画像用に補正
+          #print(sample.shape) (32, 1, 32, 32, 32)
 
-        with tf.io.gfile.GFile(
-            os.path.join(this_sample_dir, "sample.png"), "wb") as fout:
-          save_image(image_grid, fout)
+          for i in range(10):
+            for j in range(32):
+              Image.fromarray(sample[i][0][j]).save(this_sample_dir + "/" + str(i+1) + "_channel" + str(j+1) + ".png")
+        else:
+          ema.store(score_model.parameters())
+          ema.copy_to(score_model.parameters())
+          sample, n = sampling_fn(score_model)
+          ema.restore(score_model.parameters())
+          this_sample_dir = os.path.join(sample_dir, "iter_{}".format(step))
+          tf.io.gfile.makedirs(this_sample_dir)
+          nrow = int(np.sqrt(sample.shape[0]))
+          image_grid = make_grid(sample, nrow, padding=2)
+          sample = np.clip(sample.permute(0, 2, 3, 1).cpu().numpy() * 255, 0, 255).astype(np.uint8)
+          with tf.io.gfile.GFile(
+              os.path.join(this_sample_dir, "sample.np"), "wb") as fout:
+            np.save(fout, sample)
+
+          with tf.io.gfile.GFile(
+              os.path.join(this_sample_dir, "sample.png"), "wb") as fout:
+            save_image(image_grid, fout)
+        
